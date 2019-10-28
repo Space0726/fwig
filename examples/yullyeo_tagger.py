8c7c6f7881300e6b9e7880f7255f9243e7561a51
Space <space.sjwoo@gmail.com>
2019-10-28 17:14:21 +0900

    Finish yullyeo_tagger

--- examples/yullyeo_tagger.py
+++ examples/yullyeo_tagger.py
@@ -22,8 +22,11 @@ class YullyeoTagger(Uni2Kor):
             self.is_double = False
 
     def _init_char(self):
-        first_points = self.glyph.contours[0].points[0]
-        self.char = Uni2Kor.get_sound(self.points_attr[first_points].get_attr('sound'), self.char_tag)
+        initial_point = self.glyph.contours[0].points[0]
+        self.char = Uni2Kor.get_sound(
+            self.points_attr[initial_point].get_attr('sound'), 
+            self.char_tag
+        )
 
     def _init_char_tag(self):
         name = self.glyph.name
@@ -34,46 +37,45 @@ class YullyeoTagger(Uni2Kor):
         elif name.endswith('F'):
             self.char_tag = (self.code - 0xAC00) % 28
 
+    def _separate_contours(self):
+        centers = {contour:_calc_center(contour.points[0])[0] for contour in self.glyph.contours}
+        separated_dict = {}
+        center_len = len(centers.values())
+        if center_len == 2 or center_len == 4:
+            contours = sorted(centers.items(), key=lambda x: x[1])
+            separated_dict['left'] = [contour[0] for i, contour in enumerate(contours)
+                                                 if i < center_len / 2]
+            separated_dict['right'] = [contour[0] for i, contour in enumerate(contours)
+                                                  if i >= center_len / 2]
+        if separated_dict:
+            for attr, contours in separated_dict.items():
+                for contour in contours:
+                    self.points_attr[contour.points[0]].add_attr('double', attr)
+
     def name2code(self, name):
         return int(name.split('_')[0], 16)
 
-    # def add_tag(self):
-    #     for point, attr in self.points_attr.items():
-    #         if point.index == 0:
-    #             attr.add_attr('char', self.char_tag)
-    #             if self.is_double:
-    #                 sound = attr.get_attr('sound')
-    #                 center_x = _calc_center(point)[0]
-    #                 if (sound == 'final' and center_x < 290) or (sound == 'first' and center_x < 0):
-    # TODO: use _separate_contours()
-    #                     attr.add_attr('double', 'left')
-    #                 else:
-    #                     attr.add_attr('double', 'right')
+    def add_tag(self):
+        points_attr = list(self.points_attr.items())
+        if self.is_double:
+            self._separate_contours()
+        for point, attr in points_attr:
+            attr.add_attr('char', self.char_tag)
+            if self.is_double and attr.get_attr('double') is None:
+                sound = attr.get_attr('sound')
+                center_x = _calc_center(point)[0]
+                if sound == 'final':
+                    if center_x < 290:
+                        attr.add_attr('double', 'left')
+                    else:
+                        attr.add_attr('double', 'right')
 
 
 def _calc_center(point):
     box = point.contour.box
     return (box[0] + box[2]) / 2, (box[1] + box[3]) / 2
 
-def _separate_contours(glyph):
-    centers = {contour:_calc_center(contour.points[0])[0] for contour in glyph.contours}
-    separated_dict = {}
-    center_len = len(centers.keys())
-    if center_len == 2 or center_len == 4:
-        contours = sorted(centers.items(), key=lambda x: x[1])
-        separated_dict['left'] = [contour for i, contour in enumerate(contours)
-                                          if i < center_len / 2]
-        separated_dict['right'] = [contour for i, contour in enumerate(contours)
-                                           if i >= center_len / 2]
 
-    return separated_dict
-    
-
-glyph = CurrentGlyph()
-point = glyph.contours[0].points[0]
-yt = YullyeoTagger(glyph)
-print(yt.char_tag)
-print(yt.char)
-print(yt.is_double)
-print(point.contour.box)
-print(yt._calc_center(point))
+if __name__ == "__main__":
+    yt = YullyeoTagger(CurrentGlyph())
+    yt.add_tag()
