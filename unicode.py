2120624b4a12b7467cb67b5e3157a1b16914c37b
Space <space.sjwoo@gmail.com>
2019-10-28 17:58:54 +0900

    Modify docstring

--- unicode.py
+++ unicode.py
@@ -1,4 +1,4 @@
-""" 클래스 인자로 유니코드(16진수, 10진수 상관없이) 숫자를 넣어주면 해당하는 글자의 구성요소를 얻을 수 있음
+""" Convert unicode to character and letter components.
 
 초성: first_char, 중성: middle_char, 종성: final_char
 0xAC00: '가' ~ 0xD7A3: '힣'
@@ -10,25 +10,30 @@ Created by Seongju Woo.
 class Uni2Kor():
     """ Convert unicode to Hangul(Korean) characters.
 
-    Examples:
-        uc = Uni2Char(0xAC00)
-        print(uc) # 글자 구성요소 출력 -> 'AC00: ㄱ + ㅏ'
+    Args:
+        code:: int (default is None)
+            A unicode value in Korean range. (0xAC00 ~ 0xD7A3)
 
-        # Two usages.
-        Uni2Char(0xAC00).get_chars()    # Save characters information to object.
-        Uni2Char().get_chars(0xAC00)    # Doesn't save, just convert it.
+    Examples:
+        >>> uc = Uni2Char(0xAC00)
+        >>> uc    # Print letter components
+        'AC00: ㄱ + ㅏ'
+        >>> Uni2Char(0xAC00).get_chars()    # Save characters information to object.
+        ['ㄱ', 'ㅏ', None]
+        >>> Uni2Char().get_chars(0xAC00)    # Doesn't save, just convert it.
+        ['ㄱ', 'ㅏ', None]
     """
-    # 588개씩
+    # 588 each
     first = {
         0:'ㄱ', 1:'ㄲ', 2:'ㄴ', 3:'ㄷ', 4:'ㄸ', 5:'ㄹ', 6:'ㅁ', 7:'ㅂ', 8:'ㅃ', 9:'ㅅ', 10:'ㅆ',
         11:'ㅇ', 12:'ㅈ', 13:'ㅉ', 14:'ㅊ', 15:'ㅋ', 16:'ㅌ', 17:'ㅍ', 18:'ㅎ'
     }
-    # 28개씩
+    # 28 each
     middle = {
         0:'ㅏ', 1:'ㅐ', 2:'ㅑ', 3:'ㅒ', 4:'ㅓ', 5:'ㅔ', 6:'ㅕ', 7:'ㅖ', 8:'ㅗ', 9:'ㅘ', 10:'ㅙ',
         11:'ㅚ', 12:'ㅛ', 13:'ㅜ', 14:'ㅝ', 15:'ㅞ', 16:'ㅟ', 17:'ㅠ', 18:'ㅡ', 19:'ㅢ', 20:'ㅣ'
     }
-    # 28개씩
+    # 28 each
     final = {
         0:None, 1:'ㄱ', 2:'ㄲ', 3:'ㄱㅅ', 4:'ㄴ', 5:'ㄴㅈ', 6:'ㄴㅎ', 7:'ㄷ', 8:'ㄹ', 9:'ㄹㄱ',
         10:'ㄹㅁ', 11:'ㄹㅂ', 12:'ㄹㅅ', 13:'ㄹㅌ', 14:'ㄹㅍ', 15:'ㄹㅎ', 16:'ㅁ', 17:'ㅂ', 18:'ㅂㅅ',
@@ -66,8 +71,8 @@ class Uni2Kor():
             matched character:: str
 
         Examples:
-            print(Uni2Kor.get_sound('first', 0))
-            # Result is 'ㄱ'
+            >>> Uni2Kor.get_sound('first', 0)
+            'ㄱ'
         """
         if sound_name == 'first':
             return self.first[value]
@@ -79,13 +84,25 @@ class Uni2Kor():
             raise ValueError("Invalid sound name")
 
     def get_hex_code(self):
-        """ Returns Unicode in hexadecimal format. """
+        """ Returns Unicode in hexadecimal format.
+
+        Returns:
+            unicode in hexadecimal format:: str
+        """
         if self.code is None:
             return ""
         return hex(self.code).upper()[2:]
 
     def get_chars(self, code=None):
-        """ Returns characters that converted from unicode. """
+        """ Returns letter component of character that converted from unicode.
+
+        Args:
+            code:: int (default is None)
+                A unicode that you want to convert.
+
+        Returns:
+            letter component of character:: [str, str, str(or None)]
+        """
         if code is None:
             return [self.first_char, self.middle_char, self.final_char]
         first_idx = (code - 0xAC00) // 588
@@ -94,7 +111,15 @@ class Uni2Kor():
         return [Uni2Kor.first[first_idx], Uni2Kor.middle[middle_idx], Uni2Kor.final[final_idx]]
 
     def get_char_dict(self, to_hex=False):
-        """ 유니코드 및 글자 구성요소들을 딕셔너리 자료형으로 리턴 """
+        """ Returns dictionary of unicode and letter component
+
+        Args:
+            to_hex:: bool (default is False)
+                True if you want to return unicode in hexadecimal, otherwise False
+
+        Returns:
+            unicode(keys) and letter component(values):: { str: [str, str, str(or None)] }
+        """
         if self.code is None:
             return {}
         if to_hex:
