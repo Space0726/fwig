818c7e88faa23692d287e6e2f8e0355d9010f1c6
Space <space.sjwoo@gmail.com>
2019-11-28 16:05:22 +0900

    Add yullyeo_stroke example

---
+++ examples/yullyeo_stroke.py
@@ -0,0 +1,9 @@
+from stemfont.attributing.stroke import add_stroke_attr
+from stemfont.tools import iterfont
+from fontParts.world import CurrentFont
+
+def need_stroke(glyph):
+    return not glyph.name.startswith('uni')
+
+if __name__ == '__main__':
+    iterfont.glyph_generator(CurrentFont(), add_stroke_attr, add_stroke_attr=need_stroke)
