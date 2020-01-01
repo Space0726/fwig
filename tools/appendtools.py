36c85e1631b376c51d131c6f49f0ead058a41f15
Space0726 <space.sjwoo@gmail.com>
2020-01-02 04:27:31 +0900

    Modify tools/appendtools.py for coding convention

--- tools/appendtools.py
+++ tools/appendtools.py
@@ -1,6 +1,6 @@
 """ Font tools for appending point at line or curve object.
 
-Last modified date: 2019/10/07
+Last modified date: 2020/01/02
 
 Created by Jeongjae Suk.
 Modified by Seongju Woo.
@@ -8,129 +8,123 @@ Modified by Seongju Woo.
 from fontTools.misc.bezierTools import splitCubic, splitCubicAtT, splitLine
 
 def append_point_coordinate(contour, rpoints, where, is_horizontal):
-    """ 곡선을 x 혹은 y 값을 기준으로 나누어 점을 추가하는 함수.
+    """ Appends RPoint object to curve(RSegment object) by horizontal or vertical line.
 
     Args:
-        contour:: RContour object
-            robofont에서 사용하는 contour object인 RContour.
-        rpoints:: [RPoint object, ...]
-            robofont에서 사용하는 point object인 RPoint의 list[곡선의 시작점, 보조점, 보조점, 곡선의 끝점]
-            순으로 구성 필요, 곡선의 시작점은 contour의 방향상 가장 먼저 나오는 점.
+        contour:: RContour
+            The RContour object that you want to add RPoint object.
+        rpoints:: list
+            A list of RPoint objects. It should be containing 4 RPoint objects like
+            [startpoint, bcp(of startpoint), bcp(of endpoint), endpoint].
+            The order of start and end follows a index of contour.points.
         where:: int or float
-            나눌 기준이 될 x 혹은 y 값.
+            The coordinate value of line(x value if uses vertical line otherwise y value).
         is_horizontal:: bool
-            where이 x 값이면 False, y 값이면 True.
+            If this is True, uses horizontal line(coordinate value of y).
+            Otherwise uses vertical line(coordinate value of x).
 
-    Returns:
-        status flag:: int
-            1이면 정상 종료, -1이면 에러 발생.
+    Raises:
+        arguments value error:: ValueError
+            If not found target segment in contour, raises this error.
+            This can be occured when the rpoints(RPoint objects) are not in the contour.points.
+        splitting error:: AssertionError
+            If function of splitting is not done properly, raises this error.
+            For example, if it split one line(or curve) but result is also one line(or curve).
     """
     points = _r2t(rpoints)
     new_curve = splitCubic(points[0], points[1], points[2], points[3], where, is_horizontal)
-
-    if len(new_curve) < 2:
-        return -1
-
-    if _append_point_curve(contour, rpoints, new_curve):
-        return -1
-
-    return 1
+    assert(len(new_curve) > 1)
+    _append_point_curve(contour, rpoints, new_curve)
 
 def append_point_rate(contour, rpoints, rate):
-    """ 곡선을 비율로 나누어 점을 추가하는 함수.
-    
-    Args:
-        contour:: RContour object
-            robofont에서 사용하는 contour object인 RContour.
-        rpoints:: [RPoint object, ...]
-            robofont에서 사용하는 point object인 RPoint의 list[곡선의 시작점, 보조점, 보조점, 곡선의 끝점]
-            순으로 구성 필요, 곡선의 시작점은 contour의 방향상 가장 먼저 나오는 점.
-        rate:: float
-            0 ~ 1 사이의 나눌 비율.
+    """ Appends RPoint object to curve(RSegment object) by rate.
 
-    Returns:
-        status flag:: int
-            1이면 정상 종료, -1이면 에러 발생.
+    Args:
+        contour:: RContour
+            The RContour object that you want to add RPoint object.
+        rpoints:: list
+            A list of RPoint objects. It should be containing 4 RPoint objects like
+            [startpoint, bcp(of startpoint), bcp(of endpoint), endpoint].
+            The order of start and end follows a index of contour.points.
+        rate:: float
+            A rate of location. It must be in [0, 1].
+
+    Raises:
+        arguments value error:: ValueError
+            If not found target segment in contour, raises this error.
+            This can be occured when the rpoints(RPoint objects) are not in the contour.points.
+        splitting error:: AssertionError
+            If function of splitting is not done properly, raises this error.
+            For example, if it split one line(or curve) but result is also one line(or curve).
     """
     points = _r2t(rpoints)
     new_curve = splitCubicAtT(points[0], points[1], points[2], points[3], rate)
-
-    if len(new_curve) < 2:
-        return -1
-
-    if _append_point_curve(contour, rpoints, new_curve) < 0:
-        return -1
-
-    return 1
+    assert(len(new_curve) > 1)
+    _append_point_curve(contour, rpoints, new_curve)
 
 def _append_point_curve(contour, rpoints, curve):
     segment_index = _segment_index_of(contour, rpoints[3])
-    if segment_index < 0:
-        return -1
-
+    if segment_index is None:
+        raise ValueError('Not found target segment in contour.')
     _change_point(contour[segment_index].offCurve[0], curve[1][1])
     _change_point(contour[segment_index].offCurve[1], curve[1][2])
-
     contour.insertSegment(segment_index, 'curve', list(curve[0][1:]))
-
     contour.round()
 
-    return 1
-
 def append_point_coordinate_line(contour, rpoints, where, is_horizontal):
-    """ 직선을 x 혹은 y 값을 기준으로 나누어 점을 추가하는 함수.
+    """ Appends RPoint object to line(RSegment object) by horizontal or vertical line.
 
     Args:
-        contour:: RContour object
-            robofont에서 사용하는 contour object인 RContour.
-        rpoints:: [RPoint object, ...]
-            robofont에서 사용하는 point object인 RPoint의 list[직선의 시작점, 직선의 끝점]
-            순으로 구성 필요, 직선의 시작점은 contour의 방향상 가장 먼저 나오는 점.
+        contour:: RContour
+            The RContour object that you want to add RPoint object.
+        rpoints:: list
+            A list of RPoint objects. It should be containing startpoint and endpoint of line.
+            The order of start and end follows a index of contour.points.
         where:: int or float
-            나눌 기준이 될 x 혹은 y 값.
+            The coordinate value of line(x value if uses vertical line otherwise y value).
         is_horizontal:: bool
-            where이 x 값이면 False, y 값이면 True.
+            If this is True, uses horizontal line(coordinate value of y).
+            Otherwise uses vertical line(coordinate value of x).
 
-    Returns:
-        status flag:: int
-            1이면 정상 종료, -1이면 에러 발생.
+    Raises:
+        arguments value error:: ValueError
+            If not found target segment in contour, raises this error.
+            This can be occured when the rpoints(RPoint objects) are not in the contour.points.
+        splitting error:: AssertionError
+            If function of splitting is not done properly, raises this error.
+            For example, if it split one line(or curve) but result is also one line(or curve).
     """
     points = _r2t(rpoints)
     new_line = splitLine(points[0], points[1], where, is_horizontal)
-
-    if len(new_line) < 2:
-        return -1
-
+    assert(len(new_line) > 1)
     segment_index = _segment_index_of(contour, rpoints[1])
-    if segment_index < 0:
-        return -1
-
+    if segment_index is None:
+        raise ValueError('Not found target segment in contour.')
     contour.insertSegment(segment_index, 'line', [new_line[0][1]])
-
     contour.round()
 
-    return 1
-
 def append_point_rate_line(contour, rpoints, rate):
-    """ 직선을 비율로 나누어 점을 추가하는 함수.
+    """ Appends RPoint object to line(RSegment object) by rate.
 
     Args:
-        contour:: RContour object
-            robofont에서 사용하는 contour object인 RContour.
-        rpoints:: [RPoint object, ...]
-            robofont에서 사용하는 point object인 RPoint의 list[직선의 시작점, 직선의 끝점]
-            순으로 구성 필요, 직선의 시작점은 contour의 방향상 가장 먼저 나오는 점.
+        contour:: RContour
+            The RContour object that you want to add RPoint object.
+        rpoints:: list
+            A list of RPoint objects. It should be containing startpoint and endpoint of line.
+            The order of start and end follows a index of contour.points.
         rate:: float
-            0 ~ 1 사이의 나눌 비율.
+            A rate of location. It must be in [0, 1].
 
-    Returns:
-        status flag:: int
-            1이면 정상 종료, -1이면 에러 발생.
+    Raises:
+        arguments value error:: ValueError
+            If contour or rpoints is None or rate is not in [0, 1], raises this error.
+        splitting error:: AssertionError
+            If function of splitting is not done properly, raises this error.
+            For example, if it split one line(or curve) but result is also one line(or curve).
     """
 
     if not (contour and rpoints and 0 <= rate <= 1):
-        return -1
-
+        raise ValueError('Arguments contour and rpoints must not be None and rate must be in [0, 1]')
     if rpoints[0].x == rpoints[1].x:
         append_point_coordinate_line(
             contour,
@@ -153,18 +147,15 @@ def append_point_rate_line(contour, rpoints, rate):
             True
         )
 
-    return 1
-
 def _internal_division(a, b, rate):
     s = 1 - rate
     return list(map(lambda x: int(round(x)), [b.x*rate + a.x*s, b.y*rate + a.y*s]))
 
 def _segment_index_of(contour, rpoint):
-    for index in range(0, len(contour)):
-        if contour[index].onCurve.x == rpoint.x and contour[index].onCurve.y == rpoint.y:
+    for index, segment in enumerate(contour):
+        if segment.onCurve.x == rpoint.x and segment.onCurve.y == rpoint.y:
             return index
-
-    return -1
+    return None
 
 def _change_point(rpoint, point):
     rpoint.x = point[0]
@@ -172,8 +163,6 @@ def _change_point(rpoint, point):
 
 def _r2t(points):
     tpoint = []
-
     for point in points:
         tpoint.append((point.x, point.y))
-
     return tpoint
